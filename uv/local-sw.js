3501ba5fe7266e0f1fcbc87d7cfe7282cfe2a7ea
GRASSUBG <ctgames122333@gmail.com>
2026-08-28 15:13:29 -0400

    Fix local Ultraviolet service worker bootstrap

--- uv/local-sw.js
+++ uv/local-sw.js
@@ -1,1 +1,15 @@
-importScripts('/uv/uv.bundle.js', '/uv/uv.config.js', '/uv/uv.handler.js', '/uv/sw.js');
+importScripts(
+    '/uv/uv.bundle.js',
+    '/uv/uv.client.js',
+    '/uv/uv.config.js',
+    '/uv/uv.handler.js',
+    '/uv/sw.js'
+);
+
+const uv = new self.UVServiceWorker();
+
+self.addEventListener('fetch', (event) => {
+    if (uv.route(event)) {
+        event.respondWith(uv.fetch(event));
+    }
+});
