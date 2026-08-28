58cb861f29e4aa739085db25271cdd47a89f5e94
GRASSUBG <ctgames122333@gmail.com>
2026-08-28 15:35:59 -0400

    Reset Ultraviolet config while proxy is UI-only

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,12 +1,10 @@
-const localHost = location.hostname === '127.0.0.1' || location.hostname === 'localhost' || location.hostname === '::1';
-
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: localHost ? '/bare/' : 'https://tomp.app/',
+    bare: 'https://tomp.app/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
     bundle: '/uv/uv.bundle.js',
     config: '/uv/uv.config.js',
-    sw: localHost ? '/uv/local-sw.js' : '/uv/sw.js',
+    sw: '/uv/sw.js',
 };
