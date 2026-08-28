4d7c0778f38cb58041c3c0181cbe73fcedd71ea8
GRASSUBG <ctgames122333@gmail.com>
2026-08-28 14:59:25 -0400

    Use local Bare server for Ultraviolet

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://tomp.app/', // Public Bare server, can be changed if it goes down
+    bare: '/bare/', // Local Bare server provided by server.js
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
